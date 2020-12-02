bab85d06712e52df4f0012f7ef69f325593df6ec
Grzegorz Bogusz <gbogusz55@gmail.com>
2020-12-02 14:59:26 +0100

    Changes

--- ProtoG/src/Game.java
+++ ProtoG/src/Game.java
@@ -1,106 +1,91 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Random;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Game extends JFrame implements ActionListener{
+public class Game extends JComponent implements MouseListener {
 
-    private final int WIDTH = 1280;
-    private final int HEIGHT = 800;
-    private final int SQUARE_SIDE_SIDE = 200;
-
-    private int currentMolePosition;
+    private static final int DEFAULT_WIDTH = 100;
+    private static final int DEFAULT_HEIGHT = 100;
+    private int leftX = 180;
+    private int topY = 180;
+    private int moleX = 0;
+    private int moleY = 0;
     private int score = 0;
-    private JButton[] buttons;
 
-    private ImageIcon mole;
+    private JFrame frame;
 
-    private JLabel scoreLabel;
-    private JLabel welcomeMessage;
-
-    private JPanel panel;
+    private Image moleImage = new ImageIcon("src\\images\\mole1.jpg").getImage();
 
     public Game() {
-
-        setUndecorated(false);
-
-        setTitle("Whack a Mole");
-        setSize(WIDTH, HEIGHT);
-        setResizable(false);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-
-        panel = new JPanel();
-        panel.setBounds(0, 0,WIDTH, HEIGHT);
-        panel.setLayout(null);
-
-        welcomeMessage = new JLabel("Whack a Mole");
-        welcomeMessage.setFont(new Font("Arial", Font.BOLD, 48));
-        welcomeMessage.setBounds(500, 25, 400, 100);
-        panel.add(welcomeMessage);
-
-        scoreLabel = new JLabel("Score: " + score);
-        scoreLabel.setFont(new Font("Arial", Font.BOLD, 32));
-        scoreLabel.setBounds(20, 100, 200, 50);
-        panel.add(scoreLabel);
-
-        mole = new ImageIcon("src\\images\\mole.png");
-
-        buttons = new JButton[9];
-        int index = 0;
-
-        for(int i = 0; i < 3; i++)
-        {
-           for(int j = 0; j < 3; j++)
-           {
-               int x = (i * SQUARE_SIDE_SIDE) + WIDTH/2 - (SQUARE_SIDE_SIDE + SQUARE_SIDE_SIDE/2);
-               int y = (j * SQUARE_SIDE_SIDE) + HEIGHT/2 - (SQUARE_SIDE_SIDE + SQUARE_SIDE_SIDE/2);
-               buttons[index] = new JButton();
-               buttons[index].setBounds(x, y, 200, 200);
-               buttons[index].setBackground(Color.WHITE);
-               panel.add(buttons[index]);
-               buttons[index].addActionListener(this);
-               index++;
-           }
-       }
-
-        add(panel);
-        setVisible(true);
-
-       while(true) {
-           getRandomMolePosition();
-           try {
-               Thread.sleep(500 - score*3);
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
+        frame = new JFrame();
+        frame.setBounds(10, 10, 700, 700);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(new Game(1));
+        frame.setVisible(true);
     }
 
+    public Game(int i) {
+        this.addMouseListener(this);
+        this.startGame();
+    }
 
-    private void getRandomMolePosition() {
-        for(JButton button : buttons) {
-            button.setIcon(null);
-        }
-
-        Random random = new Random();
-        int n;
-        do {
-            n = random.nextInt(9);
-        } while(n == currentMolePosition);
-        buttons[n].setIcon(mole);
-        currentMolePosition = n;
+    public void startGame() {
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                moleX+=50;
+                moleY+=50;
+                repaint();
+            }
+        }, 2*1000, 2*1000);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        JButton button = (JButton) e.getSource();
-        if(button.getIcon() == mole) {
-            button.setIcon(null);
-            score++;
-            scoreLabel.setText("Score: " + score);
+    public void paintComponent(Graphics graphics) {
+
+        for(int i=0; i<=2; i++) {
+            topY+=DEFAULT_WIDTH*i;
+            for (int j=0; j<=2; j++) {
+                leftX+=DEFAULT_WIDTH*j;
+                graphics.drawRect(leftX, topY, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+                leftX=180;
+            }
+            topY=180;
+        }
+        Font f = new Font("Serif", Font.BOLD, 36);
+        Font f1 = new Font("Serif", Font.BOLD, 20);
+
+        graphics.setFont(f);
+        graphics.drawString("WHACK A MOLE", 180, 100);
+        graphics.setFont(f1);
+        graphics.drawString("Score: "+score,50, 600 );
+        graphics.drawImage(moleImage, moleX, moleY, 100, 100, null);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(e.getX()>moleX && e.getX()<=(moleX+100) && e.getY()>moleY && e.getY()<=(moleY+100)) {
+            this.score += 5;
+            repaint();
         }
     }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
 }
