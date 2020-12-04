d42955a8d8f804f5a7f5b2470f8f5501ddd2ca51
ErykHrynczuk <72573102+ErykHrynczuk@users.noreply.github.com>
2020-12-04 16:23:38 +0100

    3rd Prototype

---
+++ 3rd Prototype/src/MGS_Classes/RectS.java
@@ -0,0 +1,111 @@
+package MGS_Classes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Components contained in the Major_Frame;
+ */
+
+public class RectS extends JComponent implements MouseListener {
+
+    private static final int DEFAULT_WIDTH = 100;
+    private static final int DEFAULT_HEIGHT = 100;
+    private int leftX = 180;
+    private int topY = 180;
+    private int moleX = 0;
+    private int moleY = 0;
+    private int score = 0;
+    private int period = 2*200;
+
+    private int[]positions = new int[3];
+
+    Timer timer = new Timer();
+
+    Image moleImage = new ImageIcon("src\\mole.jpg").getImage();
+
+    public RectS() {
+        addMouseListener(this);
+        startGame();
+    }
+
+    public void startGame() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+                Random rand = new Random();
+
+                positions[0] = leftX+DEFAULT_WIDTH*0;
+                positions[1] = leftX+DEFAULT_WIDTH*1;
+                positions[2] = leftX+DEFAULT_WIDTH*2;
+
+                moleX=positions[rand.nextInt(3)];
+                moleY=positions[rand.nextInt(3)];
+                repaint();
+                period -= score;
+            }
+        }, 2*100, period);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        for(int i=0; i<=2; i++) {
+            topY+=DEFAULT_WIDTH*i;
+            for (int j=0; j<=2; j++) {
+                leftX+=DEFAULT_WIDTH*j;
+                g.drawRect(leftX, topY, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+                leftX=180;
+            }
+            topY=180;
+        }
+
+        Font f = new Font("Serif", Font.BOLD, 36);
+        Font f1 = new Font("Serif", Font.BOLD, 20);
+
+        g.setFont(f);
+        g.drawString("WHACK A MOLE", 180, 100);
+        g.setFont(f1);
+        g.drawString("Score: "+score,50, 600 );
+        g.drawImage(moleImage, moleX, moleY, 99, 99, null);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        if(e.getX()>moleX && e.getX()<=(moleX+100) && e.getY()>moleY && e.getY()<=(moleY+100)) {
+            this.score += 5;
+            repaint();
+        }
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+}
